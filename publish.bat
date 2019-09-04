0805b21f2f6d22a066c843acedeca85b77b48f28
bcssov <nxsovereign@gmail.com>
2019-09-04 04:59:40 +0200

    Comment out test publish code

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 py -3 publish.py
-cd publish
-cd merge_ai_species
-Bandizip c -r "merge_ai_species.zip" "*.*"
-xcopy "merge_ai_species.zip" "%UserProfile%\Documents\Paradox Interactive\Stellaris\mod" /y
-cd .. 
-cd ..
-cls
+REM cd publish
+REM cd merge_ai_species
+REM Bandizip c -r "merge_ai_species.zip" "*.*"
+REM xcopy "merge_ai_species.zip" "%UserProfile%\Documents\Paradox Interactive\Stellaris\mod" /y
+REM cd .. 
+REM cd ..
+REM cls
